80f3fa89ae40d4e7e7d2e66a49056134a01f2fad
Brian Shao <brian@ngsh.ca>
2023-11-26 10:29:03 -0800

    feat: add userscript

---
+++ cModLoader.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        cModLoader
+// @namespace   corru.observer
+// @match       *://*.corru.observer/*
+// @grant       none
+// @version     0.1.0
+// @author      cydq
+// @run-at      document-end
+// @description Loads the latest version of cModLoader
+// ==/UserScript==
+
+addResources(['https://cml.snowy.cafe/cModLoader.js'])
