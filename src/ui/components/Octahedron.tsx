0d7131027653351558c6f70306b92af82257730f
Brian Shao <brian@ngsh.ca>
2023-11-29 19:50:37 -0800

    feat: megahedron

--- src/ui/components/Octahedron.tsx
+++ src/ui/components/Octahedron.tsx
@@ -3,6 +3,40 @@ import "./Octahedron.css";
 export function Octahedron(props: { onClick?: () => void }) {
   return (
     <div className="octahedron" style={{}} onClick={props.onClick}>
+      <div className="solid3">
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+      </div>
+
+      <div className="solid2">
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+        <div className="side" />
+      </div>
+
       <div className="solid">
         <div className="side" />
         <div className="side" />
